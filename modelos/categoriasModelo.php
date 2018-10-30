4f0761b775994b4850a9af7f3ab833c123e798d8
Daniel Rojas <derojas85@misena.edu.co>
2018-10-30 09:54:59 -0500

    Borrar categoria, edita articulo

--- modelos/categoriasModelo.php
+++ modelos/categoriasModelo.php
@@ -101,5 +101,25 @@ class ModeloCategorias{
 
 	}
 
+	static public function mdlBuscarArticuloCategoria($tabla, $item, $valor){
+
+		if($item != null){
+
+			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item");
+
+			$stmt -> bindParam(":".$item, $valor, PDO::PARAM_STR);
+
+			$stmt -> execute();
+
+			return $stmt -> fetchAll();
+
+		}
+
+		$stmt -> close();
+
+		$stmt = null;
+
+	}
+
 }
 
